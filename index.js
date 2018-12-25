d1dd7ddd46c7347907f7bdfd9af3ba3f064262be
Wladimir Reinaga <tekkenucv@gmail.com>
2018-12-25 16:25:56 -0400

    inicial release

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+require('dotenv').config({ path: '/opt/settings/.env'})
+
+const app = express()
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.telegram.setWebhook('https://back-end-telegram-node.herokuapp.com')
+
+app
+.use(bot.webhookCallback('/'))
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'pug')
+.get('/', (req,res) => res.send('Holass!'))
+.listen(PORT, () => {
+    console.log('Listening en el puerto', PORT)
+})
+
+// funcionamiento del bot
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('ok'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.startPolling()
